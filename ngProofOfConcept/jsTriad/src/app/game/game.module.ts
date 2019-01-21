e1b7d6a671baa2bbcacba7613287d18d65eeb1f7
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2019-01-21 15:17:51 -0700

    Wired up views within game to their own components.

--- ngProofOfConcept/jsTriad/src/app/game/game.module.ts
+++ ngProofOfConcept/jsTriad/src/app/game/game.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { GameComponent } from './game.component';
+import { GameComponent }     from './game.component';
+import { PlayerComponent }   from './player/player.component';
+import { OpponentComponent } from './opponent/opponent.component';
+import { BoardComponent}     from './board/board.component';
 
 import { GameRoutingModule } from './game-routing.module';
 
 @NgModule({
   imports:[ CommonModule, GameRoutingModule ],
   exports:[],
-  declarations:[ GameComponent ],
+  declarations:[ GameComponent, PlayerComponent, BoardComponent, OpponentComponent ],
   providers:[]
 })
 
