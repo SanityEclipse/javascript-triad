3eb955889306c42bca34ffe83885e2af01352dcd
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2019-01-21 18:39:35 -0700

    Added asset for cards, tweaked layout.

---
+++ ngProofOfConcept/jsTriad/src/app/layout/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component }  from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-header',
+  template :`
+    <header style="border-bottom: 1px solid black">
+      <div class="container">
+        <div class="page-header text-center">
+          <h1>JavaScript Triad
+          </h1>
+        </div>
+      </div>
+    </header>
+`
+})
+
+export class HeaderComponent{};
